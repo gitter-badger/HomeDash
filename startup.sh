406225246ccf8a6aafd521d39e5b62f48c057a34
Paul Fauchon <p.fauchon@gmail.com>
2015-01-02 18:33:43 +0800

    Startup script

--- startup.sh
+++ startup.sh
@@ -6,6 +6,10 @@ FULLDIR=$DIR"/target/universal/stage"
 
 echo $FULLDIR
 
+chmod +x $DIR/activator
+
+chmod +x $DIR/activator clean stage
+
 kill -9 $(cat $FULLDIR/RUNNING_PID)
 
 rm $FULLDIR/RUNNING_PID
@@ -13,8 +17,6 @@ rm $FULLDIR/RUNNING_PID
 
 chmod +x $FULLDIR/bin/homedash
 
-chmod +x $FULLDIR/activator
 
-chmod +x $FULLDIR/activator clean stage
 
 nohup $FULLDIR/bin/homedash -Dhttp.port=9000 -DapplyEvolutions.default=true  -DapplyDownEvolutions.default=true -J-Xms64M -J-Xmx128M >/dev/null 2>&1 &
