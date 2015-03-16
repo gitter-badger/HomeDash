f9a5b921f6787b048229f31a40417e08e979f745
lamarios <p.fauchon@gmail.com>
2015-03-16 20:57:20 +0800

    removing bin folder

--- bin/evolutions/default/2.sql
+++
@@ -1,7 +0,0 @@
-# --- !Ups
-
-ALTER TABLE `module` ADD `remote` INT(1) DEFAULT 0
-
-# --- !Downs
-
-ALTER TABLE `module` DROP COLUMN `remote`;
--- bin/evolutions/default/4.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-
-ALTER TABLE `page` ENGINE=MyISAM;
-ALTER TABLE`module` ENGINE=MyISAM;
-ALTER TABLE `module_setting` ENGINE=MyISAM;
-ALTER TABLE `setting` ENGINE=MyISAM;
-
-# --- !Downs
-
